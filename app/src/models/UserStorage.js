5f6fbcdd6503e2386af67e22876a4ae9e7250b68
DavidJeonKr <dudgh7410@naver.com>
2022-04-06 15:02:17 +0900

    유저 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -4,19 +4,28 @@ class UserStorage {
     static #users = {
         id: ["woorimIT", "나개발", "김팀장", "1234"],
         psword: ["1234", "1234", "123456", "1234"],
-        name: ["우리밋", "나개발", "김팀장", "1234"]
+        name: ["우리밋", "나개발", "김팀장", "1234"],
     };
 
-    static getUsers(... fileds) {
-        console.log(fileds);
+    static getUsers(...fields) {
         const users = this.#users;
-        const newUsers = fields.reduce((newUsers, filed) => {
-            if(users.hasOwnProperty(field)) {
-                newUsers[filed] = users[filed];
+        const newUsers = fields.reduce((newUsers, field) => {
+            if (users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
             }
             return newUsers;
         }, {});
         return newUsers;
+    }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userInfo = Object.keys(users).reduce((newUsers, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;            
+        }, {});
+        return userInfo;
     }
 }
 
