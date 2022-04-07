9d6b26ee2df653365cb8e980357a7c51d430a0bb
DavidJeonKr <dudgh7410@naver.com>
2022-04-07 19:02:56 +0900

    winston 프로젝트에 적용

--- app/app.js
+++ app/app.js
@@ -3,8 +3,9 @@
 // 모듈
 const express = require("express");
 const bodyParser = require("body-parser")
+// const morgan = require("morgan");
+
 const dotenv = require("dotenv");
-const morgan = require("morgan");
 
 
 
@@ -25,7 +26,7 @@ app.use(bodyParser.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({extended: true}));
 //app.use(morgan("dev"));
-app.use(morgan("common", {stream : accessLogStream }));
+//app.use(morgan("tiny", {stream : logger.stream })); 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메소드.
 
 module.exports = app;
